38b01c40ce0b8e605c18f7afdc472acd2d37422e
iqra <abdikarimiqra@gmail.com>
2025-03-04 21:40:33 +0300

    add index.js

---
+++ index.js
@@ -0,0 +1,49 @@
+// 1 + 80; //=> 81
+// 60 - 40; //=> 20
+// 2 * 3.4; //=> 6.8
+// 5.0 / 2.5; //=> 2
+function add(a, b){
+    return a + b
+
+    
+}
+add (1600, 84);
+
+function subtract(a,b){
+    return a - b
+    
+}
+subtract(364, 100);
+
+function multiply(a, b){
+    return a * b
+    
+}
+multiply(294196,1)
+
+function divide(a, b){
+    return a / b
+    
+}
+divide(1.072258064516129,1);
+
+function increment(n){
+    return n + 1
+}
+increment(6)
+
+function decrement(n){
+    return n - 1
+}
+decrement(7)
+
+function makeInt(n) {
+    return parseInt(n, 10);
+  }
+
+  function preserveDecimal(n) {
+    return parseFloat(n);
+  }
+
+  
+  
